cd120daa28d3af151b94df44c30cf727fbbfaec3
Tim Riley <tim@finergames.com>
2018-07-21 11:20:02 -0400

    - updated editor extensions

--- Assets/Plugins/EditorExtensions/Editor/EditorExtensions.cs
+++ Assets/Plugins/EditorExtensions/Editor/EditorExtensions.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-using UniRx;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+#if UniRx
+using UniRx;
+#endif
 
 public static class EditorExtensions
 {
     public static readonly RectOffset DefaultPadding = new RectOffset(5, 5, 5, 5);
     public static readonly GUIStyle DefaultBoxStyle = new GUIStyle(GUI.skin.box) { padding = DefaultPadding };
 
-	public static float lineHeight = EditorGUIUtility.singleLineHeight;
-	public static float lineSpacing = EditorGUIUtility.standardVerticalSpacing;
-	public static float elementPadding = EditorGUIUtility.standardVerticalSpacing;
+    public static float lineHeight = EditorGUIUtility.singleLineHeight;
+    public static float lineSpacing = EditorGUIUtility.standardVerticalSpacing;
+    public static float elementPadding = EditorGUIUtility.standardVerticalSpacing;
 
     public static void UseVerticalBoxLayout(this Editor editor, Action action)
     {
@@ -44,7 +47,7 @@ public static class EditorExtensions
 
     public static void WithLabel(this Editor editor, string label)
     {
-		EditorGUILayout.LabelField (label, EditorStyles.boldLabel, GUILayout.MinWidth (0));
+        EditorGUILayout.LabelField(label, EditorStyles.boldLabel, GUILayout.MinWidth(0));
     }
 
     public static bool WithIconButton(this Editor editor, string icon)
@@ -56,8 +59,8 @@ public static class EditorExtensions
     {
         EditorGUILayout.BeginHorizontal();
         editor.WithLabel(label);
-//            EditorGUILayout.(value);
-		editor.WithLabel(value);
+        //EditorGUILayout.(value);
+        editor.WithLabel(value);
         EditorGUILayout.EndHorizontal();
     }
 
@@ -70,103 +73,105 @@ public static class EditorExtensions
         return result;
     }
 
-	public static bool TryDrawValue(this Editor editor, Rect rect, Type type, ref object value, string memberName, int index)
-	{
-		if (memberName == "hideFlags" || memberName == "name" || type.Name == "CompositeDisposable" || type.Name == "IDisposable")
-		{
-			return false;
-		}
+    public static bool TryDrawValue(this Editor editor, Rect rect, Type type, ref object value, string memberName, int index)
+    {
+        if (memberName == "hideFlags" || memberName == "name" || type.Name == "CompositeDisposable" || type.Name == "IDisposable")
+        {
+            return false;
+        }
 
-		if (type == typeof(int))
-		{
-			value = EditorGUI.IntField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (int)value);
-		}
-		else if (type == typeof(IntReactiveProperty))
-		{
-			var reactiveProperty = value as IntReactiveProperty;
-			reactiveProperty.Value = EditorGUI.IntField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(float))
-		{
-			value = EditorGUI.FloatField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (float)value);
-		}
-		else if (type == typeof(FloatReactiveProperty))
-		{
-			var reactiveProperty = value as FloatReactiveProperty;
-			reactiveProperty.Value = EditorGUI.FloatField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(bool))
-		{
-			value = EditorGUI.Toggle(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (bool)value);
-		}
-		else if (type == typeof(BoolReactiveProperty))
-		{
-			var reactiveProperty = value as BoolReactiveProperty;
-			reactiveProperty.Value = EditorGUI.Toggle(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(string))
-		{
-			value = EditorGUI.TextField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (string)value);
-		}
-		else if (type == typeof(StringReactiveProperty))
-		{
-			var reactiveProperty = value as StringReactiveProperty;
-			reactiveProperty.Value = EditorGUI.TextField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(Vector2))
-		{
-			value = EditorGUI.Vector2Field(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Vector2)value);
-		}
-		else if (type == typeof(Vector2ReactiveProperty))
-		{
-			var reactiveProperty = value as Vector2ReactiveProperty;
-			value = EditorGUI.Vector2Field(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(Vector3))
-		{
-			value = EditorGUI.Vector3Field(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Vector3)value);
-		}
-		else if (type == typeof(Vector3ReactiveProperty))
-		{
-			var reactiveProperty = value as Vector3ReactiveProperty;
-			value = EditorGUI.Vector3Field(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(Color))
-		{
-			value = EditorGUI.ColorField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Color)value);
-		}
-		else if (type == typeof(ColorReactiveProperty))
-		{
-			var reactiveProperty = value as ColorReactiveProperty;
-			reactiveProperty.Value = EditorGUI.ColorField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, reactiveProperty.Value);
-		}
-		else if (type == typeof(Bounds))
-		{
-			value = EditorGUI.BoundsField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Bounds)value);
-		}
-		else if (type == typeof(BoundsReactiveProperty))
-		{
-			var reactiveProperty = value as BoundsReactiveProperty;
-			reactiveProperty.Value = EditorGUI.BoundsField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Bounds)reactiveProperty.Value);
-		}
-		else if (type == typeof(Rect))
-		{
-			value = EditorGUI.RectField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Rect)value);
-		}
-		else if (type == typeof(RectReactiveProperty))
-		{
-			var reactiveProperty = value as RectReactiveProperty;
-			reactiveProperty.Value = EditorGUI.RectField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Rect)reactiveProperty.Value);
-		}
-		else if (type == typeof(Enum))
-		{
-			value = EditorGUI.EnumPopup(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Enum)value);
-		}
-		else if (type == typeof(UnityEngine.GameObject))
-		{
-			value = EditorGUI.ObjectField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (UnityEngine.GameObject)value, type, true);
-		}
-		else
+        if (type == typeof(int))
+        {
+            value = DrawInt(rect, value, memberName, index);
+        }
+        else if (type == typeof(float))
+        {
+            value = DrawFloat(rect, value, memberName, index);
+        }
+        else if (type == typeof(bool))
+        {
+            value = DrawBool(rect, value, memberName, index);
+        }
+        else if (type == typeof(string))
+        {
+            value = DrawString(rect, value, memberName, index);
+        }
+        else if (type == typeof(Vector2))
+        {
+            value = DrawVector2(rect, value, memberName, index);
+        }
+        else if (type == typeof(Vector3))
+        {
+            value = DrawVector3(rect, value, memberName, index);
+        }
+        else if (type == typeof(Color))
+        {
+            value = DrawColor(rect, value, memberName, index);
+        }
+        else if (type == typeof(Bounds))
+        {
+            value = DrawBounds(rect, value, memberName, index);
+        }
+        else if (type == typeof(Rect))
+        {
+            value = DrawRect(rect, value, memberName, index);
+        }
+        else if (type == typeof(Enum))
+        {
+            value = EditorGUI.EnumPopup(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Enum)value);
+        }
+        else if (type == typeof(UnityEngine.GameObject))
+        {
+            value = EditorGUI.ObjectField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (UnityEngine.GameObject)value, type, true);
+        }
+#if UniRx
+        else if (type == typeof(IntReactiveProperty))
+        {
+            var reactiveProperty = value as IntReactiveProperty;
+            reactiveProperty.Value = DrawInt(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(FloatReactiveProperty))
+        {
+            var reactiveProperty = value as FloatReactiveProperty;
+            reactiveProperty.Value = DrawFloat(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(BoolReactiveProperty))
+        {
+            var reactiveProperty = value as BoolReactiveProperty;
+            reactiveProperty.Value = DrawBool(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(StringReactiveProperty))
+        {
+            var reactiveProperty = value as StringReactiveProperty;
+            reactiveProperty.Value = DrawString(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(Vector2ReactiveProperty))
+        {
+            var reactiveProperty = value as Vector2ReactiveProperty;
+            value = DrawVector2(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(Vector3ReactiveProperty))
+        {
+            var reactiveProperty = value as Vector3ReactiveProperty;
+            value = DrawVector3(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(ColorReactiveProperty))
+        {
+            var reactiveProperty = value as ColorReactiveProperty;
+            reactiveProperty.Value = DrawColor(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(BoundsReactiveProperty))
+        {
+            var reactiveProperty = value as BoundsReactiveProperty;
+            reactiveProperty.Value = DrawBounds(rect, reactiveProperty.Value, memberName, index);
+        }
+        else if (type == typeof(RectReactiveProperty))
+        {
+            var reactiveProperty = value as RectReactiveProperty;
+            reactiveProperty.Value = DrawRect(rect, reactiveProperty.Value, memberName, index);
+        }
+#endif
+        else
 		{
 			Debug.LogWarning("This type is not supported: " + type.Name + " - In member: " + memberName);
 			//Debug.LogWarning("This type is not supported!");
@@ -176,7 +181,52 @@ public static class EditorExtensions
 		return true;
 	}
 
-	public static void SetHideFlags(this Editor editor, PrefabType prefabType)
+    private static int DrawInt(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.IntField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (int)value);
+    }
+
+    private static float DrawFloat(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.FloatField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (float)value);
+    }
+
+    private static bool DrawBool(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.Toggle(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (bool)value);
+    }
+
+    private static string DrawString(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.TextField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (string)value);
+    }
+
+    private static Vector2 DrawVector2(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.Vector2Field(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Vector2)value);
+    }
+
+    private static Vector3 DrawVector3(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.Vector3Field(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Vector3)value);
+    }
+
+    private static Color DrawColor(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.ColorField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Color)value);
+    }
+
+    private static Bounds DrawBounds(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.BoundsField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Bounds)value);
+    }
+
+    private static Rect DrawRect(Rect rect, object value, string memberName, int index)
+    {
+        return EditorGUI.RectField(new Rect(rect.x, rect.y + (index * lineHeight) + (index * lineSpacing), rect.width, lineHeight), memberName, (Rect)value);
+    }
+
+    public static void SetHideFlags(this Editor editor, PrefabType prefabType)
 	{
 		if (prefabType == PrefabType.None)
 		{
